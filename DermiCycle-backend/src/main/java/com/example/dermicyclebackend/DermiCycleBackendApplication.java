0f83d5aab97d62998423a20004fcfe21325d1f4f
avrubio <ariadna.vanegas@gmail.com>
2023-10-18 16:08:18 -0400

    Spring initualization project structure added

---
+++ DermiCycle-backend/src/main/java/com/example/dermicyclebackend/DermiCycleBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.dermicyclebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DermiCycleBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DermiCycleBackendApplication.class, args);
+	}
+
+}
---
+++ DermiCycle-backend/src/test/java/com/example/dermicyclebackend/DermiCycleBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dermicyclebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DermiCycleBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
