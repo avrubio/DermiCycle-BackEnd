f57be25b7d94f4d4612685ce5aa66a9756aee403
avrubio <ariadna.vanegas@gmail.com>
2023-10-19 12:58:50 -0400

    instance fields added to UserService, constructor added to UserService class

--- DermiCycle-backend/src/main/java/com/example/dermicyclebackend/service/UserService.java
+++ DermiCycle-backend/src/main/java/com/example/dermicyclebackend/service/UserService.java
@@ -1,8 +1,26 @@
 package com.example.dermicyclebackend.service;
 
 
+import com.example.dermicyclebackend.repository.UserRepository;
+import com.example.dermicyclebackend.security.JwtUtils;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserService {
+    private final UserRepository userRepository;
 
+    private final PasswordEncoder passwordEncoder;
+
+   private final JwtUtils jwtUtils;
+   private AuthenticationManager authenticationManager;
+
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder, JwtUtils jwtUtils, AuthenticationManager authenticationManager) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtUtils = jwtUtils;
+        this.authenticationManager = authenticationManager;
+    }
 }
 
