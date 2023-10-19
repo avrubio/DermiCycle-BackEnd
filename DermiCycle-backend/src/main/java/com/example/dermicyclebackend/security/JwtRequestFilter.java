43c1f8e0b8bc550d932ec1d337364536d8e8857b
avrubio <ariadna.vanegas@gmail.com>
2023-10-19 15:38:07 -0400

    parseJwt method added to JwtRequestFilter class

--- DermiCycle-backend/src/main/java/com/example/dermicyclebackend/security/JwtRequestFilter.java
+++ DermiCycle-backend/src/main/java/com/example/dermicyclebackend/security/JwtRequestFilter.java
@@ -3,7 +3,9 @@ package com.example.dermicyclebackend.security;
 import com.example.dermicyclebackend.service.MyUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.logging.Logger;
 
 @Component
@@ -24,6 +26,20 @@ public class JwtRequestFilter {
     @Autowired
     public void setJwtUtils(JwtUtils jwtUtils) {
         this.jwtUtils = jwtUtils;
+    }
+
+    /**
+     * Parse the JWT token from the request's Authorization header.
+     *
+     * @param request The HTTP request.
+     * @return The JWT token or null if not found.
+     */
+    private String parseJwt(HttpServletRequest request) {
+        String headerAuth = request.getHeader("Authorization");
+        if (StringUtils.hasText(headerAuth) && headerAuth.startsWith("Bearer ")) {
+            return headerAuth.substring(7, headerAuth.length());
+        }
+        return null;
     }
 
 }
