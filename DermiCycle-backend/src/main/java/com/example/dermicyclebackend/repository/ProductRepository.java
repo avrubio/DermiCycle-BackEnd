f75296963c64c40f987cbaa9a3a7b5b340997a69
avrubio <ariadna.vanegas@gmail.com>
2023-10-19 16:09:10 -0400

    ProductRepository extending JpaRepository and set up method to findByUserIdAndEmailAddress and findByUserId

--- DermiCycle-backend/src/main/java/com/example/dermicyclebackend/repository/ProductRepository.java
+++ DermiCycle-backend/src/main/java/com/example/dermicyclebackend/repository/ProductRepository.java
@@ -1,4 +1,12 @@
 package com.example.dermicyclebackend.repository;
 
-public interface ProductRepository {
+import com.example.dermicyclebackend.models.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    Product findByUserIdAndEmailAddress(Long userId, String emailAddress);
+
+    Product findByUserId(Long userId);
 }
