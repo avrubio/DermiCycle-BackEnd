d881f894e892bca59a1c329fc1057f71178c0631
avrubio <ariadna.vanegas@gmail.com>
2023-10-19 15:37:15 -0400

    JwtRequstFilter class set up with instance fields and setJwtUtils

--- DermiCycle-backend/src/main/java/com/example/dermicyclebackend/security/JwtRequestFilter.java
+++ DermiCycle-backend/src/main/java/com/example/dermicyclebackend/security/JwtRequestFilter.java
@@ -1,4 +1,29 @@
 package com.example.dermicyclebackend.security;
 
-public class JwtRequestfilter {
+import com.example.dermicyclebackend.service.MyUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.logging.Logger;
+
+@Component
+public class JwtRequestFilter {
+    Logger logger = Logger.getLogger(JwtRequestFilter.class.getName());
+
+    @Autowired
+    private MyUserDetailsService myUserDetailsService;
+
+    @Autowired
+    private JwtUtils jwtUtils;
+
+    /**
+     * Set the JWT utility class.
+     *
+     * @param jwtUtils The JWT utility class to set.
+     */
+    @Autowired
+    public void setJwtUtils(JwtUtils jwtUtils) {
+        this.jwtUtils = jwtUtils;
+    }
+
 }
