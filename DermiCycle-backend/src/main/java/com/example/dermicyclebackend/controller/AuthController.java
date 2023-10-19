3b22a847ec11dda410638e9ca6bad021587dce58
avrubio <ariadna.vanegas@gmail.com>
2023-10-19 13:02:35 -0400

    AuthController annotations added, UserService added as instance field

--- DermiCycle-backend/src/main/java/com/example/dermicyclebackend/controller/AuthController.java
+++ DermiCycle-backend/src/main/java/com/example/dermicyclebackend/controller/AuthController.java
@@ -1,4 +1,17 @@
 package com.example.dermicyclebackend.controller;
 
+import com.example.dermicyclebackend.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path ="/auth/users/")
 public class AuthController {
+
+    private UserService userService;
+@Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
 }
