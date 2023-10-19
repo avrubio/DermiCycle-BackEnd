193849fca529bcf5a759255f1d077555491b6b56
avrubio <ariadna.vanegas@gmail.com>
2023-10-19 15:44:46 -0400

    UserControllerTest class instance fields set up and annotations added:

--- DermiCycle-backend/src/test/java/com/example/dermicyclebackend/controller/UserControllerTest.java
+++ DermiCycle-backend/src/test/java/com/example/dermicyclebackend/controller/UserControllerTest.java
@@ -1,6 +1,37 @@
 package com.example.dermicyclebackend.controller;
 
+import com.example.dermicyclebackend.service.MyUserDetailsService;
+import com.example.dermicyclebackend.service.UserService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import net.bytebuddy.implementation.bind.annotation.RuntimeType;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@ComponentScan(basePackages = "com.example")
 public class UserControllerTest {
+
+    @Autowired
+    @MockBean
+    private UserService userService;
+
+    @MockBean
+    private MyUserDetailsService myUserDetailsService;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
 
 
 }
