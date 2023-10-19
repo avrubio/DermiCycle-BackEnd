f204683f62b420dfe960ae0681ad18e1be4273df
avrubio <ariadna.vanegas@gmail.com>
2023-10-19 12:55:27 -0400

    findUserByEmailAddress method added to UserRepository interface

--- DermiCycle-backend/src/main/java/com/example/dermicyclebackend/repository/UserRepository.java
+++ DermiCycle-backend/src/main/java/com/example/dermicyclebackend/repository/UserRepository.java
@@ -1,4 +1,25 @@
 package com.example.dermicyclebackend.repository;
 
-public interface UserRepository {
+import com.example.dermicyclebackend.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+/**
+ * Repository interface for managing Doctor entities using Spring Data JPA.
+ */
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    /**
+     * Find a user by their email address.
+     *
+     * @param emailAddress The email address of the user to find.
+     * @return The user with the specified email address or null if not found.
+     */
+    User findUserByEmailAddress(String emailAddress);
+    /**
+     * Check if a user with a given email address exists.
+     *
+     * @param emailAddress The email address to check for existence.
+     * @return True if a user with the email address exists, false otherwise.
+     */
+    boolean existsByEmailAddress(String emailAddress);
 }
--- DermiCycle-backend/src/main/java/com/example/dermicyclebackend/service/UserService.java
+++ DermiCycle-backend/src/main/java/com/example/dermicyclebackend/service/UserService.java
@@ -1,6 +1,8 @@
 package com.example.dermicyclebackend.service;
 
 
+
 public class UserService {
 
+}
 
