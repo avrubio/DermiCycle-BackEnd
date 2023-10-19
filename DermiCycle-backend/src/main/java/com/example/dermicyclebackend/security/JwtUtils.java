fd58ae388ffeeb9a197f3f0dbdf34f1ddc1602b1
avrubio <ariadna.vanegas@gmail.com>
2023-10-19 15:18:13 -0400

    JwtUtils instance fields added to class

--- DermiCycle-backend/src/main/java/com/example/dermicyclebackend/security/JwtUtils.java
+++ DermiCycle-backend/src/main/java/com/example/dermicyclebackend/security/JwtUtils.java
@@ -1,7 +1,17 @@
 package com.example.dermicyclebackend.security;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.util.logging.Logger;
 
 @Service
 public class JwtUtils {
+    Logger logger = Logger.getLogger(JwtUtils.class.getName());
+
+    @Value("${jwt-secret}")
+    private String jwtSecret;
+
+    @Value("${jwt-expiration-ms}")
+    private int jwtExpirationMs;
 }
