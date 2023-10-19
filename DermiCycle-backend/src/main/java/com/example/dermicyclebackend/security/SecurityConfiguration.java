2beb71742f632ff83705862aa1b1a87a64a2d77f
avrubio <ariadna.vanegas@gmail.com>
2023-10-19 15:09:41 -0400

    SecurityFilterChain method added to SecurityConfiguration class

--- DermiCycle-backend/src/main/java/com/example/dermicyclebackend/security/SecurityConfiguration.java
+++ DermiCycle-backend/src/main/java/com/example/dermicyclebackend/security/SecurityConfiguration.java
@@ -3,7 +3,10 @@ package com.example.dermicyclebackend.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
@@ -16,6 +19,18 @@ public class SecurityConfiguration{
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/auth/users/register/").permitAll()
+                .antMatchers("/h2-console/**").permitAll()
+                .anyRequest().authenticated()
+                .and().sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and().csrf().disable()
+                .headers().frameOptions().disable();
+        return http.build();
     }
 
 }
