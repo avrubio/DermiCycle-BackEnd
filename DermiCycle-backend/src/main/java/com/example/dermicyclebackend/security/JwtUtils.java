4bb650a3015b80748fa13e92a2f9c58180ea0881
avrubio <ariadna.vanegas@gmail.com>
2023-10-19 15:19:34 -0400

    generateJwtToken method added to JwtUtils class

--- DermiCycle-backend/src/main/java/com/example/dermicyclebackend/security/JwtUtils.java
+++ DermiCycle-backend/src/main/java/com/example/dermicyclebackend/security/JwtUtils.java
@@ -1,8 +1,11 @@
 package com.example.dermicyclebackend.security;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.logging.Logger;
 
 @Service
@@ -14,4 +17,18 @@ public class JwtUtils {
 
     @Value("${jwt-expiration-ms}")
     private int jwtExpirationMs;
+    /**
+     * Generate a JWT token based on the provided user details.
+     *
+     * @param myUserDetails The user details to include in the JWT.
+     * @return The generated JWT token.
+     */
+    public String generateJwtToken(MyUserDetails myUserDetails) {
+        return Jwts.builder()
+                .setSubject(myUserDetails.getEmailAddress()) // Just the user email
+                .setIssuedAt(new Date())
+                .setExpiration(new Date((new Date()).getTime() + jwtExpirationMs))
+                .signWith(SignatureAlgorithm.HS256, jwtSecret)
+                .compact();
+    }
 }
