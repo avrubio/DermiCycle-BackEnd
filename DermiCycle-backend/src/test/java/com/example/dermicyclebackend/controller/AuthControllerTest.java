c3c2d4f2242f9115198b2e9cb679f730447bb52a
avrubio <ariadna.vanegas@gmail.com>
2023-10-19 13:38:38 -0400

    AuthControllerTest class set up and createUser test created and tested

--- DermiCycle-backend/src/test/java/com/example/dermicyclebackend/controller/AuthControllerTest.java
+++ DermiCycle-backend/src/test/java/com/example/dermicyclebackend/controller/AuthControllerTest.java
@@ -1,4 +1,67 @@
 package com.example.dermicyclebackend.controller;
 
+import com.example.dermicyclebackend.models.User;
+import com.example.dermicyclebackend.repository.UserRepository;
+import com.example.dermicyclebackend.service.UserService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.MediaType;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.hamcrest.core.IsNull.notNullValue;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@ComponentScan(basePackages = "com.example")
 public class AuthControllerTest {
+
+    @Autowired
+    @MockBean
+    private UserService userService;
+
+    @InjectMocks
+    private UserController userController;
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Mock
+    private PasswordEncoder passwordEncoder;
+
+    User RECORD_1 = new User(1L, "ariadna@ga.com", "password123");
+
+    @Test
+    public void createUser() throws Exception {
+        when(userService.createUser(Mockito.any(User.class))).thenReturn(RECORD_1);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/auth/user/register/")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(new ObjectMapper().writeValueAsString(RECORD_1)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", notNullValue()))
+                .andExpect(jsonPath("$.id").value((RECORD_1.getId())))
+                .andExpect(jsonPath("$.emailAddress").value(RECORD_1.getEmailAddress()))
+                .andDo(print());
+    }
+
 }
