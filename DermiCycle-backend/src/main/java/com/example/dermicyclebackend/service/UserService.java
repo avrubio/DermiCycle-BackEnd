94cdf68dd9b8770ccece3d00533e09a2eeddfceb
avrubio <ariadna.vanegas@gmail.com>
2023-10-19 13:41:28 -0400

    createUser method completed

--- DermiCycle-backend/src/main/java/com/example/dermicyclebackend/service/UserService.java
+++ DermiCycle-backend/src/main/java/com/example/dermicyclebackend/service/UserService.java
@@ -1,6 +1,7 @@
 package com.example.dermicyclebackend.service;
 
 
+import com.example.dermicyclebackend.exception.InformationExistException;
 import com.example.dermicyclebackend.models.User;
 import com.example.dermicyclebackend.repository.UserRepository;
 import com.example.dermicyclebackend.security.JwtUtils;
@@ -25,6 +26,13 @@ public class UserService {
     }
 
     public User createUser(User userObject) {
+        if(!userRepository.existsByEmailAddress(userObject.getEmailAddress())) {
+            userObject.setPassword(passwordEncoder.encode(userObject.getPassword()));
+            return userRepository.save(userObject);
+
+        }else {
+            throw new InformationExistException("user with email address " + userObject.getEmailAddress() + " already exists!")
+        }
     }
 }
 
