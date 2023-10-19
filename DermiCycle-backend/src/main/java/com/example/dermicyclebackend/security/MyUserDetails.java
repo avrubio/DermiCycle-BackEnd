678cfacef1b8cbf977cf1859cd4d7560e0d924c7
avrubio <ariadna.vanegas@gmail.com>
2023-10-19 15:33:41 -0400

    MyUserDetails implementing UserDetails and set up of constructors from UerDetails

--- DermiCycle-backend/src/main/java/com/example/dermicyclebackend/security/MyUserDetails.java
+++ DermiCycle-backend/src/main/java/com/example/dermicyclebackend/security/MyUserDetails.java
@@ -1,21 +1,30 @@
 package com.example.dermicyclebackend.security;
-
 import com.example.dermicyclebackend.models.User;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.HashSet;
 
+/**
+ * Custom UserDetails implementation for Doctor entities.
+ */
 public class MyUserDetails implements UserDetails {
+
     private final User user;
 
+    /**
+     * Constructs a new MyUserDetails instance with the provided User entity.
+     *
+     * @param user The User entity for which to create UserDetails.
+     */
     public MyUserDetails(User user) {
         this.user = user;
     }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return new HashSet<>(); // You can define authorities if needed.
     }
 
     @Override
@@ -30,22 +39,31 @@ public class MyUserDetails implements UserDetails {
 
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return false;
+        return true;
     }
 
+    /**
+     * Get the User entity associated with this UserDetails.
+     *
+     * @return The User entity.
+     */
+    public User getUser() {
+        return user;
+    }
 }
+
