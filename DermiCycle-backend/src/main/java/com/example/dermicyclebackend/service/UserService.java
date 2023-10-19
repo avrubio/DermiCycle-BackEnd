a0546daeac1ecea8de6e0ede4051f295c4e9344f
avrubio <ariadna.vanegas@gmail.com>
2023-10-19 15:16:58 -0400

    loginUser method created in UserService

--- DermiCycle-backend/src/main/java/com/example/dermicyclebackend/service/UserService.java
+++ DermiCycle-backend/src/main/java/com/example/dermicyclebackend/service/UserService.java
@@ -4,12 +4,19 @@ package com.example.dermicyclebackend.service;
 import com.example.dermicyclebackend.exception.InformationExistException;
 import com.example.dermicyclebackend.models.User;
 import com.example.dermicyclebackend.repository.UserRepository;
+import com.example.dermicyclebackend.request.LoginRequest;
 import com.example.dermicyclebackend.security.JwtUtils;
+import com.example.dermicyclebackend.security.MyUserDetails;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -34,6 +41,19 @@ public class UserService {
 
         }else {
             throw new InformationExistException("user with email address " + userObject.getEmailAddress() + " already exists!");
+        }
+    }
+
+    public Optional<String> loginUser(LoginRequest loginRequest) {
+        UsernamePasswordAuthenticationToken authenticationToken = new
+                UsernamePasswordAuthenticationToken(loginRequest.getEmailAddress(), loginRequest.getPassword());
+        try {
+            Authentication authentication = authenticationManager.authenticate(authenticationToken);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            MyUserDetails myUserDetailsDetails = (MyUserDetails) authentication.getPrincipal();
+            return Optional.of(jwtUtils.generateJwtToken(myUserDetails));
+        } catch (Exception e) {
+            return Optional.empty();
         }
     }
 }
