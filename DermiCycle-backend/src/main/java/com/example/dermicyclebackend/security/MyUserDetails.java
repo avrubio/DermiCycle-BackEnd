9356341241505b786d5c6a8634d83fc3fb0c397f
avrubio <ariadna.vanegas@gmail.com>
2023-10-19 15:21:37 -0400

    MyUserDetails class instance fields added and implementing security UserDetails

--- DermiCycle-backend/src/main/java/com/example/dermicyclebackend/security/MyUserDetails.java
+++ DermiCycle-backend/src/main/java/com/example/dermicyclebackend/security/MyUserDetails.java
@@ -1,4 +1,51 @@
 package com.example.dermicyclebackend.security;
 
-public class MyUserDetails {
+import com.example.dermicyclebackend.models.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+public class MyUserDetails implements UserDetails {
+    private final User user;
+
+    public MyUserDetails(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmailAddress();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+
 }
