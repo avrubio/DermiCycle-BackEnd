edcbe53539479ed63ec36764fa2d8496235ac3ea
avrubio <ariadna.vanegas@gmail.com>
2023-10-19 13:42:56 -0400

    InformationNotFoundException class completed

--- DermiCycle-backend/src/main/java/com/example/dermicyclebackend/exception/InformationNotFoundException.java
+++ DermiCycle-backend/src/main/java/com/example/dermicyclebackend/exception/InformationNotFoundException.java
@@ -1,4 +1,21 @@
 package com.example.dermicyclebackend.exception;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
-public class InformationNotFoundException {
+/**
+ * This exception is thrown when an attempt to retrieve information results in a not found scenario.
+ *
+ * @ResponseStatus(HttpStatus.NOT_FOUND) indicates that when this exception is thrown, it results in an HTTP 404 Not Found response status.
+ */
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class InformationNotFoundException extends RuntimeException {
+
+    /**
+     * Constructs a new InformationNotFoundException with the specified error message.
+     *
+     * @param message The error message that describes the not found scenario or the reason for the exception.
+     */
+    public InformationNotFoundException(String message) {
+        super(message);
+    }
 }
