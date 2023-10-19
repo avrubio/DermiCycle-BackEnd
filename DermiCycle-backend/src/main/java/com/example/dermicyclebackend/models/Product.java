8390f9c6e2dbcab7fb459288324a84c212f9d622
avrubio <ariadna.vanegas@gmail.com>
2023-10-19 16:11:51 -0400

    Product class set up with instance fields, getters and setters, no arg constructor, args constructor

--- DermiCycle-backend/src/main/java/com/example/dermicyclebackend/models/Product.java
+++ DermiCycle-backend/src/main/java/com/example/dermicyclebackend/models/Product.java
@@ -1,4 +1,57 @@
 package com.example.dermicyclebackend.models;
 
+import javax.persistence.*;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "products")
 public class Product {
+    @Id
+    private Long id;
+
+    private String name;
+    private String directions;
+
+    public Product() {
+    }
+
+    public Product(Long id, String name, String directions) {
+        this.id = id;
+        this.name = name;
+        this.directions = directions;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDirections() {
+        return directions;
+    }
+
+    public void setDirections(String directions) {
+        this.directions = directions;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", directions='" + directions + '\'' +
+                '}';
+    }
 }
