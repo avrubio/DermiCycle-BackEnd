319cd6cb3fb0be63f8f189d6d4d9c05067777b54
avrubio <ariadna.vanegas@gmail.com>
2023-10-19 15:09:06 -0400

    @Table annotation added to User class

--- DermiCycle-backend/src/main/java/com/example/dermicyclebackend/models/User.java
+++ DermiCycle-backend/src/main/java/com/example/dermicyclebackend/models/User.java
@@ -3,8 +3,10 @@ package com.example.dermicyclebackend.models;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "users")
 public class User {
 
     @Id
