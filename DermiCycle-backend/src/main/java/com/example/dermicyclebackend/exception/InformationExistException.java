ea78a802d185ef0dd4c06e3878a92b173cdd06ef
avrubio <ariadna.vanegas@gmail.com>
2023-10-19 13:42:27 -0400

    Information Exist Exception class completed

--- DermiCycle-backend/src/main/java/com/example/dermicyclebackend/exception/InformationExistException.java
+++ DermiCycle-backend/src/main/java/com/example/dermicyclebackend/exception/InformationExistException.java
@@ -1,4 +1,21 @@
 package com.example.dermicyclebackend.exception;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+/**
+ * This exception is thrown when an attempt to create or modify information results in a conflict
+ * because the information already exists.
+ *
+ * @ResponseStatus(HttpStatus.CONFLICT) indicates that when this exception is thrown, it results in an HTTP 409 Conflict response status.
+ */
+@ResponseStatus(HttpStatus.CONFLICT)
+public class InformationExistException extends RuntimeException {
 
-public class InformationExistException {
+    /**
+     * Constructs a new InformationExistException with the specified error message.
+     *
+     * @param message The error message that describes the conflict or the reason for the exception.
+     */
+    public InformationExistException(String message) {
+        super(message);
+    }
 }
