e96e6b3bb23ba7dfdf8274afdc9a61a771dfb331
avrubio <ariadna.vanegas@gmail.com>
2023-10-20 14:42:49 -0400

    StateRepository created and set up

--- DermiCycle-backend/src/main/java/com/example/dermicyclebackend/repository/StageRepository.java
+++ DermiCycle-backend/src/main/java/com/example/dermicyclebackend/repository/StageRepository.java
@@ -1,2 +1,9 @@
-package com.example.dermicyclebackend.repository;public interface StageRepository {
+package com.example.dermicyclebackend.repository;
+
+import com.example.dermicyclebackend.models.Stage;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StageRepository extends JpaRepository<Stage, Long> {
 }
