16b26dae932150a5d357399db83a55f93035807f
avrubio <ariadna.vanegas@gmail.com>
2023-10-20 14:41:41 -0400

    Stage model set up with instance fields and getters and setters and constructor:

--- DermiCycle-backend/src/main/java/com/example/dermicyclebackend/models/Stage.java
+++ DermiCycle-backend/src/main/java/com/example/dermicyclebackend/models/Stage.java
@@ -1,4 +1,89 @@
 package com.example.dermicyclebackend.models;
 
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "stages")
 public class Stage {
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String description;
+
+    @Column
+    private String length;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "product_id", referencedColumnName = "id")
+    private Product product;
+
+
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+
+    public Stage() {
+    }
+
+    public Stage(String name, String description, String length) {
+        this.name = name;
+        this.description = description;
+        this.length = length;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLength() {
+        return length;
+    }
+
+    public void setLength(String length) {
+        this.length = length;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
