9737689eca722fc8e2a7e3a904287995cbb9dca3
avrubio <ariadna.vanegas@gmail.com>
2023-10-24 17:16:27 -0400

    RegisterRequest javadocs added

--- DermiCycle-backend/src/main/java/com/example/dermicyclebackend/request/RegisterRequest.java
+++ DermiCycle-backend/src/main/java/com/example/dermicyclebackend/request/RegisterRequest.java
@@ -1,5 +1,8 @@
 package com.example.dermicyclebackend.request;
 
+/**
+ * Represents a request for user registration in the DermiCycle system.
+ */
 public class RegisterRequest {
     private String emailAddress;
     private String firstName;
@@ -7,9 +10,21 @@ public class RegisterRequest {
     private String skinType;
     private String password;
 
+    /**
+     * Default constructor for the RegisterRequest class.
+     */
     public RegisterRequest() {
     }
 
+    /**
+     * Parameterized constructor for the RegisterRequest class.
+     *
+     * @param emailAddress         The email address of the user.
+     * @param firstName            The first name of the user.
+     * @param skinCyclePreference  The skin cycle preference of the user.
+     * @param skinType             The skin type of the user.
+     * @param password             The password of the user.
+     */
     public RegisterRequest(String emailAddress, String firstName, String skinCyclePreference, String skinType, String password) {
         this.emailAddress = emailAddress;
         this.firstName = firstName;
@@ -18,42 +33,92 @@ public class RegisterRequest {
         this.password = password;
     }
 
+    /**
+     * Get the email address of the user.
+     *
+     * @return The email address of the user.
+     */
     public String getEmailAddress() {
         return emailAddress;
     }
 
+    /**
+     * Set the email address of the user.
+     *
+     * @param emailAddress The email address of the user.
+     */
     public void setEmailAddress(String emailAddress) {
         this.emailAddress = emailAddress;
     }
 
+    /**
+     * Get the first name of the user.
+     *
+     * @return The first name of the user.
+     */
     public String getFirstName() {
         return firstName;
     }
 
+    /**
+     * Set the first name of the user.
+     *
+     * @param firstName The first name of the user.
+     */
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
 
+    /**
+     * Get the skin cycle preference of the user.
+     *
+     * @return The skin cycle preference of the user.
+     */
     public String getSkinCyclePreference() {
         return skinCyclePreference;
     }
 
+    /**
+     * Set the skin cycle preference of the user.
+     *
+     * @param skinCyclePreference The skin cycle preference of the user.
+     */
     public void setSkinCyclePreference(String skinCyclePreference) {
         this.skinCyclePreference = skinCyclePreference;
     }
 
+    /**
+     * Get the skin type of the user.
+     *
+     * @return The skin type of the user.
+     */
     public String getSkinType() {
         return skinType;
     }
 
+    /**
+     * Set the skin type of the user.
+     *
+     * @param skinType The skin type of the user.
+     */
     public void setSkinType(String skinType) {
         this.skinType = skinType;
     }
 
+    /**
+     * Get the password of the user.
+     *
+     * @return The password of the user.
+     */
     public String getPassword() {
         return password;
     }
 
+    /**
+     * Set the password of the user.
+     *
+     * @param password The password of the user.
+     */
     public void setPassword(String password) {
         this.password = password;
     }
