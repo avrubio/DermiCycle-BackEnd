3b8d8c9d0b57c359559612aeef898cc349740342
avrubio <ariadna.vanegas@gmail.com>
2023-10-24 13:07:00 -0400

    ProductRepository updated

--- DermiCycle-backend/src/main/java/com/example/dermicyclebackend/repository/ProductRepository.java
+++ DermiCycle-backend/src/main/java/com/example/dermicyclebackend/repository/ProductRepository.java
@@ -5,9 +5,13 @@ import com.example.dermicyclebackend.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
     Product findByNameAndUser(String productName, User user);
 
     Product findByUserId(Long userId);
+
+    List<Product> findByStageIdAndUser(Long stageId,User user);
 }
