9e4fbc17d6d3e34507634073a3897ed2ae249e1d
avrubio <ariadna.vanegas@gmail.com>
2023-10-24 17:06:44 -0400

    added more javadocs to UserController

--- DermiCycle-backend/src/main/java/com/example/dermicyclebackend/controller/UserController.java
+++ DermiCycle-backend/src/main/java/com/example/dermicyclebackend/controller/UserController.java
@@ -30,13 +30,14 @@ public class UserController {
         this.userService = userService;
     }
 
-
     static HashMap<String, Object> message = new HashMap<>();
 
     /**
-     * Endpoint for creating a new product under the current user
+     * Endpoint for creating a new product under the current user.
      *
-     * @return A ResponseEntity indicating success or conflict along with a message and data.
+     * @param productWithStageObject The ProductWithStage object containing product details.
+     * @return A ResponseEntity indicating success (HTTP 201 - Created) or conflict (HTTP 409 - Conflict)
+     * along with a message and data.
      */
     @PostMapping(path = "/products/")
     public ResponseEntity<?> createProductUser(@RequestBody ProductWithStage productWithStageObject) {
@@ -51,23 +52,34 @@ public class UserController {
         }
     }
 
+    /**
+     * Endpoint for getting a list of products in a specific stage for the current user.
+     *
+     * @param stageId The ID of the stage for which products are to be retrieved.
+     * @return A ResponseEntity indicating success (HTTP 200 - OK) or not found (HTTP 404 - Not Found)
+     * along with a message and data.
+     */
     @GetMapping(path = "/stage/{stageId}/")
     public ResponseEntity<?> getSingleStageForUser(@PathVariable(value = "stageId") Long stageId) {
         try {
             List<SingleProductResponseWithStageForUser> productList = userService.getSingleStageForUser(stageId);
             message.put("message", "success");
             message.put("data", productList);
-            return new ResponseEntity<>(message, HttpStatus.CREATED);
+            return new ResponseEntity<>(message, HttpStatus.OK);
         } catch (InformationNotFoundException e) {
             message.put("message", "There are no products currently in this stage " + stageId);
             return new ResponseEntity<>(message, HttpStatus.NOT_FOUND);
         }
     }
 
+    /**
+     * Endpoint for a simple "Hello" message.
+     *
+     * @return A ResponseEntity with a "success" message and HTTP 201 - Created status.
+     */
     @GetMapping("/hello")
     public ResponseEntity<?> hello() {
         message.put("message", "success");
         return new ResponseEntity<>(message, HttpStatus.CREATED);
     }
-
 }
