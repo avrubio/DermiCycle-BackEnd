29a105be1f1e508c81e9eecd387edbd2adbfd4e4
avrubio <ariadna.vanegas@gmail.com>
2023-10-24 13:09:27 -0400

    ProfileResponse created to be able to retrieve userProfile first name and skin type

--- DermiCycle-backend/src/main/java/com/example/dermicyclebackend/response/ProfileResponse.java
+++ DermiCycle-backend/src/main/java/com/example/dermicyclebackend/response/ProfileResponse.java
@@ -1,2 +1,30 @@
-package com.example.dermicyclebackend.response;public class ProfileResponse {
+package com.example.dermicyclebackend.response;
+
+public class ProfileResponse {
+    private String firstName;
+    private String skinType;
+
+    public ProfileResponse() {
+    }
+
+    public ProfileResponse(String firstName, String skinType) {
+        this.firstName = firstName;
+        this.skinType = skinType;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSkinType() {
+        return skinType;
+    }
+
+    public void setSkinType(String skinType) {
+        this.skinType = skinType;
+    }
 }
