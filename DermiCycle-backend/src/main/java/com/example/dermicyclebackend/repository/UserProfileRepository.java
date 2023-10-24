95d83ed8a084577d7065592307d724ace33da7cf
avrubio <ariadna.vanegas@gmail.com>
2023-10-24 17:14:18 -0400

    added javadocs to UserProfileRepository

--- DermiCycle-backend/src/main/java/com/example/dermicyclebackend/repository/UserProfileRepository.java
+++ DermiCycle-backend/src/main/java/com/example/dermicyclebackend/repository/UserProfileRepository.java
@@ -4,7 +4,17 @@ import com.example.dermicyclebackend.models.UserProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * Repository interface for managing user profiles in the DermiCycle system.
+ */
 @Repository
 public interface UserProfileRepository extends JpaRepository<UserProfile, Long> {
-UserProfile findByUserId(Long userId);
+
+    /**
+     * Find a user profile by the ID of the associated user.
+     *
+     * @param userId The ID of the user associated with the user profile.
+     * @return The user profile associated with the specified user ID, if found.
+     */
+    UserProfile findByUserId(Long userId);
 }
