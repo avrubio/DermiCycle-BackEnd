a045a075e3957d05e26e7873fb1e145423a1909e
avrubio <ariadna.vanegas@gmail.com>
2023-10-24 13:07:21 -0400

    UserProfileRepository created and updated

--- DermiCycle-backend/src/main/java/com/example/dermicyclebackend/repository/UserProfileRepository.java
+++ DermiCycle-backend/src/main/java/com/example/dermicyclebackend/repository/UserProfileRepository.java
@@ -1,2 +1,10 @@
-package com.example.dermicyclebackend.repository;public interface UserProfileRepository {
+package com.example.dermicyclebackend.repository;
+
+import com.example.dermicyclebackend.models.UserProfile;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserProfileRepository extends JpaRepository<UserProfile, Long> {
+UserProfile findByUserId(Long userId);
 }
