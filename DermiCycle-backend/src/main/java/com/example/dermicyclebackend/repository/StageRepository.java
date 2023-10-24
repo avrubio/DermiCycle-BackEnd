01bf2769f14949095f6a5815aae408d13b2ef7ce
avrubio <ariadna.vanegas@gmail.com>
2023-10-24 17:13:37 -0400

    added javadocs to StageRepository

--- DermiCycle-backend/src/main/java/com/example/dermicyclebackend/repository/StageRepository.java
+++ DermiCycle-backend/src/main/java/com/example/dermicyclebackend/repository/StageRepository.java
@@ -5,7 +5,17 @@ import com.example.dermicyclebackend.models.Stage;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * Repository interface for managing stages in the DermiCycle system.
+ */
 @Repository
 public interface StageRepository extends JpaRepository<Stage, Long> {
+
+    /**
+     * Find a stage by the ID of the associated product.
+     *
+     * @param productId The ID of the product associated with the stage.
+     * @return The stage associated with the specified product ID, if found.
+     */
     Product findByProductId(Long productId);
 }
